99452ffb6c334ab378f1b45b6e943c696c8ed18e
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-06-20 20:39:36 +0530

    Time: 4 ms (7.25%), Space: 44.3 MB (12.33%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,12 +1,20 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
         int count = 0;
-        for(int i=0; i<nums.size(); i++) {
-            for(int j=0; j<nums.size(); j++) {
-                if(i<j) {
-                if(nums.get(i) + nums.get(j) < target){
-                      count++;
-                   }
+        Collections.sort(nums); // sorted the list so that I can apply two pointers.
+
+        // According to question, constraints were small so this brute force two pointers will work.
+        for(int i=0; i<nums.size(); i++){
+            int j=nums.size()-1;
+
+            while(i<j){
+                int sum = nums.get(i) + nums.get(j);
+                if(sum < target){
+                    count++;
+                    j--;
+                }
+                else if(sum >= target){
+                    j--;
                 }
             }
         }
