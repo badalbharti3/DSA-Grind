6dc627b674dc9ba1861032128c0bddf5b38f6521
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-06-22 14:50:29 +0530

    Time: 3 ms (77.02%), Space: 69.8 MB (24.8%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -3,7 +3,7 @@ class Solution {
         int sum = 0;
         for(int i=0; i<k; i++){
             sum+=nums[i];
-        }
+        } 
         int max = sum;
         for(int i=k; i<nums.length; i++){
             sum+=nums[i];
