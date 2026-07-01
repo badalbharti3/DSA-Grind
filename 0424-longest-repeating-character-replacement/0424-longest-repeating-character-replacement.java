d986ae75130fbaf8ff236cb68b291f1a97dc254f
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-07-01 22:59:49 +0530

    Time: 8 ms (67.54%), Space: 45.9 MB (95.61%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,24 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int max = 0, mxF = 0;
-        int n = s.length();
-        int j = 0;
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            char c = s.charAt(i);
-            map.put(c,map.getOrDefault(c,0)+1);
-            mxF = Math.max(mxF, map.get(c));
-            int changes = (i - j+1) - mxF;
-            if(changes <= k){
-                max = Math.max(max, i-j+1);
-            }
-            while((i - j + 1) - mxF  > k){
-                map.put(s.charAt(j), map.get(s.charAt(j))-1);
-                if(map.get(s.charAt(j)) == 0) map.remove(s.charAt(j));
-                j++;
-            }
-        }
-        return max;
 
+        int[] freq = new int[26];
+
+        int left = 0;
+        int maxFreq = 0;
+        int ans = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'A']++;
+
+            maxFreq = Math.max(
+                    maxFreq,
+                    freq[s.charAt(right) - 'A']
+            );
+
+            while((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
     }
 }
