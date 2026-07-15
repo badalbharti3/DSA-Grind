d540701b88c44f50478b8f9904d1082d8f23215c
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-07-16 00:09:46 +0530

    Time: 2 ms (92.18%), Space: 47.9 MB (20.33%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int idx = 0,i=0;
+        while(i<nums.length){
+            if(nums[i] != 0){
+                nums[idx] = nums[i];
+                idx++;
+            }
+            i++;
+        }
+
+        while(idx < nums.length){
+            nums[idx++] = 0;
+        }
+        
+    }
+}
