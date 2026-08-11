f8383832e554016646600cbea0a4dd38656a86e6
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-08-11 22:17:26 +0530

    Time: 7 ms (64.58%), Space: 46.6 MB (61.57%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -3,18 +3,32 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
+        int[][] pre = new int[m+1][n+1];
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                pre[i][j] = mat[i-1][j-1]
+                            + pre[i-1][j]
+                            + pre[i][j-1]
+                            - pre[i-1][j-1];                 
+            }
+        }
+        
         int[][] ans = new int[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                int sum = 0;
-                for(int r=i-k; r<=i+k; r++){
-                    for(int c=j-k; c<=j+k; c++){
-                        if(c >=0 && r >= 0 && r<m && c<n){
-                            sum+=mat[r][c];
-                        }
-                    }
-                }
-                ans[i][j] = sum;
+                int r1 = Math.max(0, i-k);
+                int r2 = Math.min(m-1, i+k);
+                int c1 = Math.max(0, j-k);
+                int c2 = Math.min(n-1, j+k);
+                
+                r1++; 
+                r2++;
+                c1++;
+                c2++;
+                ans[i][j] = pre[r2][c2] 
+                            - pre[r1 - 1][c2] 
+                            - pre[r2][c1-1] 
+                            + pre[r1-1][c1-1];
             }
         }
         return ans;
