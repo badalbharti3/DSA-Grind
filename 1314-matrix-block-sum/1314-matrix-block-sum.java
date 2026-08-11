215e16deb6f679cf9ba35fd0d5fd978befe73e4b
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-08-11 16:14:38 +0530

    Time: 468 ms (15.1%), Space: 46.8 MB (41.44%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] ans = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int sum = 0;
+                for(int r=i-k; r<=i+k; r++){
+                    for(int c=j-k; c<=j+k; c++){
+                        if(c >=0 && r >= 0 && r<m && c<n){
+                            sum+=mat[r][c];
+                        }
+                    }
+                }
+                ans[i][j] = sum;
+            }
+        }
+        return ans;
+    }
+}
