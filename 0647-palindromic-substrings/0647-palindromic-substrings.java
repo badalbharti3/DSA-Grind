f696a2e025cd2d04f3c5224ef55d462ce167baec
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-08-14 22:35:21 +0530

    Time: 799 ms (15.47%), Space: 42.9 MB (55.47%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,26 @@
+class Solution {
+    private static boolean isValid(String s, int l, int r){
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+            
+        }
+        return true;
+    }
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(isValid(s, i, j)){
+                    count++;
+                }
+            }
+           
+        }
+        return count;
+    }
+}
