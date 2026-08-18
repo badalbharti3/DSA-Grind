1e5d2c61d86fa9746544afae29fb07d80ee097bd
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-08-18 23:02:53 +0530

    Time: 0 ms (100%), Space: 47.3 MB (49.77%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,18 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-
-        int left = 0, right = nums.length-1;
-
-        while(left <= right){
-            int mid = left+(right-left)/2;
-            if(nums[mid] == mid){
-                left = mid+1;
-            }
-            else {
-                right = mid-1;
-            }
+        int totalXor = 0;
+        int n = nums.length;
+        for(int i=1; i<=n; i++){
+            totalXor = totalXor ^ i;
         }
-        return left;
+
+        int realXor = 0;
+        for(int i:nums){
+            realXor ^= i;
+        }
+
+        return totalXor ^ realXor;
+
     }
 }
