a7aba8123692a04bfb9d3999c902e789bd3a9791
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-09-02 08:58:17 +0530

    Time: 5 ms (82.23%), Space: 45.9 MB (71.96%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if( x < 0) return false;
+        int temp = x;
+        long rev = 0;
+
+        while(temp != 0){
+            int rem = temp % 10;
+            rev = rev*10+rem;
+
+            temp /= 10;
+        }
+
+        if(rev == x) return true;
+        return false;
+    }
+}
