232ac6796e1d1acffbeb42ec784ad053063d2f99
Badal Bharti <158066269+badalbharti3@users.noreply.github.com>
2026-09-02 09:11:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (86.45%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int ans = 0;
+        int fact = 5;
+        while((n/fact) != 0){
+            ans += n / fact;
+            fact *= 5;
+        }
+        return ans;
+    }
+}
